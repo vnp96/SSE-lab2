81bb5d160ab7ce64c6bd3cea0ba15727eb3710f1
Vishnu Narayan <vishnu.periyamana23@imperial.ac.uk>
2023-10-30 15:21:02 +0000

    adding unit tests

--- api/app_test.py
+++ api/app_test.py
@@ -5,6 +5,10 @@ def test_knows_about_dinosaurs():
     assert process_query("dinosaurs") ==\
             "Dinosaurs ruled the Earth 200 million years ago"
 
+def test_your_name():
+    assert process_query("What is your name") ==\
+            "Vishnu"
+
 
 def test_does_not_know_about_asteroids():
     assert process_query("asteroids") == "Unknown"
