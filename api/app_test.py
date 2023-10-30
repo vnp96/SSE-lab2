2e20f2fb416873ee462750238ac7cadb5867f7a3
Vishnu Narayan <vishnu.periyamana23@imperial.ac.uk>
2023-10-30 15:37:32 +0000

    flake8 fixes:

--- api/app_test.py
+++ api/app_test.py
@@ -5,12 +5,16 @@ def test_knows_about_dinosaurs():
     assert process_query("dinosaurs") ==\
             "Dinosaurs ruled the Earth 200 million years ago"
 
+
 def test_your_name():
     assert process_query("What is your name") ==\
             "Vishnu"
 
+
 def test_largest_number():
-    assert process_query("Which of the following numbers is the largest: 21, 29, 84 ?") == 84
+    assert process_query("Which of the following numbers \
+            is the largest: 21, 29, 84 ?") == 84
+
 
 def test_does_not_know_about_asteroids():
     assert process_query("asteroids") == "Unknown"
