9cae88626978de3237ef7da3f179ef4c7a54c304
vnp23 <vnp23@imperial.ac.uk>
2024-05-02 12:17:40 +0100

    python formatting.

--- api/app.py
+++ api/app.py
@@ -26,8 +26,7 @@ def dad_api_get_joke():
     dad_joke = ""
     api_ninja_key = os.environ.get("API_NINJA_KEY")
 
-    dad_joke_response = requests.get("https://api.api-ninjas.com/v1/dadjokes"
-                                     ,
+    dad_joke_response = requests.get("https://api.api-ninjas.com/v1/dadjokes",
                                      headers={"X-Api-Key": api_ninja_key})
     if dad_joke_response.status_code == 200:
         dad_joke = dad_joke_response.json()[0]["joke"]
